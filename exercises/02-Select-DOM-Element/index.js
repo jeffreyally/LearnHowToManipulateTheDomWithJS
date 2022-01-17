3cc9162043f405ed393c5a72e4a000fd746a12e0
jeffreyally <jeffreyally@gmail.com>
2022-01-17 21:34:51 +0000

    Select DOM Element exercise 2

--- exercises/02-Select-DOM-Element/index.js
+++ exercises/02-Select-DOM-Element/index.js
@@ -1,1 +1,16 @@
-//your code here
+firsth1 = document.querySelector('#theTitle')
+firsth1.addEventListener("contextmenu", myFunction)
+
+function myFunction(){
+
+    alert('Right click alert')
+    
+}
+var newParagraphElem = document.createElement("p");
+newParagraphElem.innerHTML = '<strong>Right click the h1</strong>';
+document.body.append(newParagraphElem);
+
+
+
+
+
