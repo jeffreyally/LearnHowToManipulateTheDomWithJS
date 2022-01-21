e3949f6146103e60ce8ae2d9c2fb587068552f01
jeffreyally <jeffreyally@gmail.com>
2022-01-21 15:48:17 +0000

    solution for exercise 5 Create DOM elem second

--- exercises/06-Create-DOM-Element-Second/index.js
+++ exercises/06-Create-DOM-Element-Second/index.js
@@ -1,3 +1,3 @@
 //you code here
-document.body.innerHTML = "<h1>Hello World</h1>";
+document.body.innerHTML = "<img src='https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/mcdonalds-fries-are-offered-at-mcdonalds-at-made-in-america-news-photo-1026311378-1538173668.jpg' width' = '1000' height = '1000'>"
 
