766f879aff73e54e162e8ca9f39761a1d5d0cbc9
jeffreyally <jeffreyally@gmail.com>
2022-01-24 16:24:38 +0000

    solution for 14 todo list w/space

--- exercises/14-Todo-List/index.js
+++ exercises/14-Todo-List/index.js
@@ -7,7 +7,7 @@
 document.querySelector('#addToDo').addEventListener("change",(e)=>{
     
     newli = document.createElement('li')
-    newli.innerHTML = '<span><i class="fa fa-trash"></i></span>' + e.path[0].value
+    newli.innerHTML = '<span><i class="fa fa-trash"></i></span>' +' '+ e.path[0].value
     document.querySelector('ul').append(newli)
     listofspans = document.querySelectorAll(".fa.fa-trash")
     listofspans.forEach((span)=>{
