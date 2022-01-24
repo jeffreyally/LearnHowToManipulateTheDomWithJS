0630fae448d66d811cac38066e2e8a1c3e2d67b1
jeffreyally <jeffreyally@gmail.com>
2022-01-24 01:20:30 +0000

    solution for 9 remove dom elem part 2

--- exercises/09-Remove-DOM-Element-part-two/index.js
+++ exercises/09-Remove-DOM-Element-part-two/index.js
@@ -1,1 +1,9 @@
 
+let list = document.querySelector('#parentLi')
+
+//list.removeChild(list.childNodes[3]) removes the 2nd LI
+list.removeChild(list.children[1])
+//I think list.children gives you back an array with just the elements
+//while list.childNodes gives you back blank text elements
+//on the even indexes(0,2,4 e.t.c...) while the odd indexes have 
+//the Lis
