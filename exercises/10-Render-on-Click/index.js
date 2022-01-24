ba32fe6dc6ff552f54665565734c22882d33df08
jeffreyally <jeffreyally@gmail.com>
2022-01-24 01:28:12 +0000

    solution for 10 render on Click

--- exercises/10-Render-on-Click/index.js
+++ exercises/10-Render-on-Click/index.js
@@ -1,5 +1,8 @@
 let button = document.querySelector("#superDuperButton");
 button.addEventListener("click", function() {
-	//your code here
+	thediv = document.createElement("div")
+	thediv.style.backgroundColor = 'yellow'
+	thediv.innerHTML = 'Hello World'
+	document.body.append(thediv)
 	
 });
