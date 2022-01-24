c5a1b6f8b6f35ea56f18ad9de7003b7e5dc7ec68
jeffreyally <jeffreyally@gmail.com>
2022-01-24 15:31:43 +0000

    gi

--- exercises/14-Todo-List/index.js
+++ exercises/14-Todo-List/index.js
@@ -1,1 +1,28 @@
-// Your code here
+ 
+
+listofspans = document.querySelectorAll(".fa.fa-trash")
+
+listofspans.forEach((span)=>{
+    span.addEventListener("click",() =>
+        span.parentNode.parentNode.remove()
+    )
+
+
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//lifesaver on how to find FA icons: https://stackoverflow.com/questions/51351679/how-do-i-color-a-fontawesome-icon-with-javascript
