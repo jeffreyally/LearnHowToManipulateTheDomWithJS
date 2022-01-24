124e824647a5dd385c748784e13a2376bf033b29
jeffreyally <jeffreyally@gmail.com>
2022-01-24 16:06:31 +0000

    solution for 14 todo list

--- exercises/14-Todo-List/index.js
+++ exercises/14-Todo-List/index.js
@@ -1,19 +1,21 @@
  
 
-listofspans = document.querySelectorAll(".fa.fa-trash")
 
-listofspans.forEach((span)=>{
+//e.path[0].value
+
+
+document.querySelector('#addToDo').addEventListener("change",(e)=>{
+    
+    newli = document.createElement('li')
+    newli.innerHTML = '<span><i class="fa fa-trash"></i></span>' + e.path[0].value
+    document.querySelector('ul').append(newli)
+    listofspans = document.querySelectorAll(".fa.fa-trash")
+    listofspans.forEach((span)=>{
     span.addEventListener("click",() =>
         span.parentNode.parentNode.remove()
     )
-
-
-
 })
-
-
-
-
+})
 
 
 
