2d3c107f91ecdb1dd3ae49b25e4afec0e47d68c2
jeffreyally <jeffreyally@gmail.com>
2022-01-24 20:34:21 +0000

    revised solution for 11

--- exercises/11-Add-li-on-Click/index.js
+++ exercises/11-Add-li-on-Click/index.js
@@ -1,6 +1,16 @@
+LisInUL = 3
 let button = document.querySelector("#superDuperButton");
 button.addEventListener("click", function() {
-	newli = document.createElement('li')
-	newli.innerHTML = 'Fourth element'
+	console.log(LisInUL +=1);
+	newli = document.createElement('li');
+	newli.innerHTML = LisInUL+'th element';
+	console.log(String(LisInUL).endsWith('2'))
+	LisInUL > 20 && String(LisInUL).endsWith('1') ? newli.innerHTML = LisInUL +'st element':'false';
+	LisInUL > 21 && String(LisInUL).endsWith('2') ? newli.innerHTML = LisInUL +'nd element':'false';
+	LisInUL > 22 && String(LisInUL).endsWith('3') ? newli.innerHTML = LisInUL +'rd element':'false';
+	
+
 	document.querySelector('#myList').append(newli)
+	
 });
+ //good up until 100 I think
